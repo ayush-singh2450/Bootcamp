184b60358d4387f3c43b80fb92ae68ca7c31c30f
Ayush Singh <ayushgurudutta24@gmail.com>
2026-05-28 18:46:59 +0530

    Counting vowels in string

---
+++ Arrays/CountVowels.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i = 0, count = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while(str[i] != '\0') {
+
+        if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
+           str[i] == 'o' || str[i] == 'u' ||
+           str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
+           str[i] == 'O' || str[i] == 'U') {
+
+            count++;
+        }
+
+        i++;
+    }
+
+    printf("Number of vowels = %d", count);
+
+    return 0;
+}
