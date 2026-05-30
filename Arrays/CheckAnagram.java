d1665b5beae195672a204adfe773708a3f327509
Ayush Singh <ayushgurudutta24@gmail.com>
2026-05-30 16:20:23 +0530

    Checking Anagram

---
+++ Arrays/CheckAnagram.java
@@ -0,0 +1,36 @@
+package Arrays;
+
+import java.util.Scanner;
+
+public class CheckAnagram {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str1=sc.nextLine().toLowerCase();
+        String str2=sc.nextLine().toLowerCase();
+
+        if (str1.length() != str2.length()) {
+            System.out.println("Not Anagram");
+            return;
+        }
+
+        int[] count = new int[256];
+
+        for (int i = 0; i < str1.length(); i++) {
+            count[str1.charAt(i)]++;
+            count[str2.charAt(i)]--;
+        }
+
+        boolean isAnagram = true;
+        for (int i : count) {
+            if (i != 0) {
+                isAnagram = false;
+                break;
+            }
+        }
+
+        System.out.println(isAnagram ? "Anagram" : "Not Anagram");
+    }
+}
+
+
