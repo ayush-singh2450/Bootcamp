009eda7338d6128af4031f2efad3a02ff4ec167e
Ayush Singh <ayushgurudutta24@gmail.com>
2026-05-31 17:44:54 +0530

    Compression of String

---
+++ Arrays/StringCompress.java
@@ -0,0 +1,21 @@
+package Arrays;
+import java.util.Scanner;
+class StringCompress{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String str = sc.nextLine();
+        String compressed = "";
+        int count=1;
+        for(int i=0;i<str.length();i++){
+            while(i<str.length()-1 && str.charAt(i)==str.charAt(i+1)){
+                count++;
+                i++;
+            }
+            compressed += str.charAt(i);
+            compressed += count;
+            count=1;
+        }
+        System.out.println("Compressed String : "+ compressed);
+        sc.close();
+    }
+}
