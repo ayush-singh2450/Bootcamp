401ac13c112f84d313c4dab29338dbdb7d2d9929
Ayush Singh <ayushgurudutta24@gmail.com>
2026-06-06 20:00:43 +0530

    Finding area of largest rectangle in a histogram

---
+++ StackAndQueue/LargRectInHistogram.java
@@ -0,0 +1,40 @@
+package StackAndQueue;
+
+import java.util.*;
+
+public class LargRectInHistogram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+         int n =sc.nextInt();
+        int[] arr = new int[n];
+         for (int i = 0; i < n; i++) {
+             arr[i]=sc.nextInt();
+         }
+         Stack<Integer> s = new Stack<>();
+         int right[] = new int[n];
+         int left[] = new int[n];
+         for (int i = n-1; i>=0; i--) {
+             while(s.size()>0 && arr[s.peek()]>=arr[i])
+                s.pop();
+            right[i]=s.empty()? n:s.peek();
+            s.push(i);         
+         }
+         while(!s.empty())
+            s.pop();
+
+           for (int i = 0; i<n; i++) {
+             while(s.size()>0 && arr[s.peek()]>=arr[i])
+                s.pop();
+            left[i]=s.empty()? -1:s.peek();
+            s.push(i);         
+         }
+         int ans=0;
+         for (int i = 0; i<n; i++){
+            int currArea = arr[i] * (right[i]-left[i]-1);
+            ans=Math.max(ans,currArea);
+         }
+         System.out.print(ans);
+         sc.close();
+        }
+    
+}
